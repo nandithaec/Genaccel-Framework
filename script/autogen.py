f256bff34e3dd485af8ace7c309ce5fbfe64dc05
Jay Shah <46132046+jayshah1x@users.noreply.github.com>
2024-04-12 15:31:06 +0530

    Update autogen.py

--- script/autogen.py
+++ script/autogen.py
@@ -8,7 +8,23 @@ vector = [2, 4, 6, 8, 10]
 count = 0
 
 def execute_vivado():
-  
+    # Define the terminal command
+    command = """
+    cd /tools/Xilinx/Vivado/2023.2/bin/
+    ./vivado -source /home/iiitb/Desktop/script.tcl -mode tcl
+    """
+
+    # Run the command
+    try:
+        # Execute the command and capture the output
+        output = subprocess.check_output(command, shell=True, universal_newlines=True)
+        
+        # Print the output
+        print(output)
+        
+    except subprocess.CalledProcessError as e:
+        # If there's an error, print the error message
+        print("Error executing command:", e) 
 
 
 
