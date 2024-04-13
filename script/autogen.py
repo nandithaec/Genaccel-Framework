91ac9dce37e8068d7ee3caef440efe583aa517b7
Jay Shah <46132046+jayshah1x@users.noreply.github.com>
2024-04-13 11:43:59 +0530

    Update autogen.py

--- script/autogen.py
+++ script/autogen.py
@@ -1,12 +1,14 @@
 import subprocess
 import openpyxl
 import pandas as pd
+import os
+import shutil
 #Defining formulas
 # Throughput (matmul per second) = No. of 4x4 matmul / (one second * One Systolic Cycle)
 # Throughput (Systolic per second) = No. Of PE mac x Vector size / One second 
 # Latency = Time Taken to compute one Element ops x Total number of elements in the mat (Row A x Col A x Row B) 
 # Throughput/W = Throughput/Power 
-vector = [2, 4, 6, 8, 10]
+vector = [2, 4, 6, 8]
 count = 0
 
 
@@ -15,8 +17,14 @@ workbook = openpyxl.Workbook()
 sheet = workbook.active
 
 sheet.append(["Design", "Tile 1", "Tile 2", "Tile 3", "Tile 4"])
-def execute_vivado():
-    # Define the terminal command
+def execute_vivado(file):
+    #Making the directory
+    directory_path = "/home/iiitb/Desktop/Test_Case/design_" + str(file)
+
+    # Create the directory
+    os.makedirs(directory_path, exist_ok=True)
+
+
     command = """
     cd /tools/Xilinx/Vivado/2023.2/bin/
     ./vivado -source /home/iiitb/Desktop/script.tcl -mode tcl
@@ -32,7 +40,55 @@ def execute_vivado():
         
     except subprocess.CalledProcessError as e:
         # If there's an error, print the error message
-        print("Error executing command:", e) 
+        print("Error executing command:", e)
+
+
+    # Specify the paths of the source file and the new destination
+    source_file = "/home/iiitb/Desktop/Test_Case/power_rpt.rpt"
+    new_destination = "/home/iiitb/Desktop/Test_Case/design_" + str(file) + "/power_rpt.rpt"
+
+    # Move the file and rename it
+    shutil.move(source_file, new_destination)
+
+
+    # Specify the paths of the source file and the new destination
+    source_file = "/home/iiitb/Desktop/Test_Case/timing_rpt.rpt"
+    new_destination = "/home/iiitb/Desktop/Test_Case/design_" + str(file) + "/timing_rpt.rpt"
+
+    # Move the file and rename it
+    shutil.move(source_file, new_destination)
+
+    # Specify the paths of the source file and the new destination
+    source_file = "/home/iiitb/Desktop/Test_Case/utilization_rpt.rpt"
+    new_destination = "/home/iiitb/Desktop/Test_Case/design_" + str(file) + "/utilization_rpt.rpt"
+
+    # Move the file and rename it
+    shutil.move(source_file, new_destination)
+
+def rewrite_file(v1, v2, v3, v4):
+   # Open the file in read mode
+  with open('/home/iiitb/Jay/htiles_design_1/htiles_design_1.srcs/sources_1/new/parameters.v', 'r') as file:
+      lines = file.readlines()
+
+  # Make changes to the content
+  new_lines = []
+  var = ""
+  for line in lines:
+      # Modify the content as needed
+      if('V1' in line):
+        new_lines.append('parameter V1 = ' + str(v1) + '; \n')
+      elif('V2' in line):
+         new_lines.append('parameter V2 = ' + str(v2) + '; \n')
+      elif('V3' in line):
+         new_lines.append('parameter V3 = ' + str(v3) + '; \n')
+      elif('V4' in line):
+         new_lines.append('parameter V4 = ' + str(v4) + '; \n')
+      else:
+        new_lines.append(line)   
+
+  # Open the file in write mode to overwrite the content
+  with open('/home/iiitb/Jay/htiles_design_1/htiles_design_1.srcs/sources_1/new/parameters.v', 'w') as file:
+      file.writelines(new_lines)
 
 data = []
 
@@ -44,10 +100,16 @@ for v1 in vector:
         #running a tcl script and reading the utilization, power and timing report
         design = ["design " + str(count) , str(v1), str(v2), str(v3), str(v4)]
         sheet.append(design)
+        print("righting the design name in the xcel shit")
+        rewrite_file(v1, v2, v3, v4)
+        print("parameter file lakhi didhi")
+        execute_vivado(count)
+        print("check kar la baka")
         count = count + 1
+        print(v1, v2, v3, v4)
 
 # Save the workbook to a file
-workbook.save("my_excel_file.xlsx")
+workbook.save("/home/iiitb/Desktop/Test_Case/my_excel_file.xlsx")
 # Print a success message
 print("Excel file created successfully!") 
         
@@ -56,5 +118,4 @@ def write_parameters(v1, v2, v3, v4):
     f.write("")
     
 print(count)
-
 
